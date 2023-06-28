254a8e28e4b689039c70069d442bc3c264c7c472
Ankit Mishra <ankit926mishra@gmail.com>
2023-06-28 23:14:38 +0530

    Cronjob added to keep server running

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
-const dotenv = require("dotenv").config()
+const dotenv = require("dotenv").config();
 const express = require("express");
+const cron = require("node-cron");
 
 require("./database/db/connectDB");
 const bookingRoute = require("./Routers/bookingsRoute");
 const cors = require("cors");
 const app = express();
 
-app.use(cors({
-  origin: process.env.ORIGIN
-}))
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+  })
+);
 
 app.use(express.json());
 app.use(bookingRoute);
@@ -16,4 +19,14 @@ app.use(bookingRoute);
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
+});
+
+
+// to keep website running
+cron.schedule("*/10 * * * *", () => {
+  // console.log("Cron job is executing");
+  var exec = require("child_process").exec;
+  exec("ping book-seats-in-a-coach-app.onrender.com", function (err, stdout, stderr) {
+    // console.log(stdout);
+  });
 });
